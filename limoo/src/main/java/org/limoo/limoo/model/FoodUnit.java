91fa773793e25bbc8a9f0179c9cbfd32f2cba09d
erfannoury <erfannoury@github.com>
2014-07-08 15:01:57 +0430

    Basic implementation of the FoodUnit class

--- limoo/src/main/java/org/limoo/limoo/model/FoodUnit.java
+++ limoo/src/main/java/org/limoo/limoo/model/FoodUnit.java
@@ -5,5 +5,58 @@ package org.limoo.limoo.model;
  */
 public class FoodUnit
 {
+    private double energy;
+    private String unitName;
+    private double multiplier;
+
+    /*
+     Constructor for FoodUnit
+     */
+    public FoodUnit(String name, double energy, double multiplier)
+    {
+        this.unitName = name;
+        this.energy = energy;
+        this.multiplier = 0 < multiplier? multiplier : 0.0;
+    }
+
+    /*
+     Constructor for base units
+     */
+    public FoodUnit(String name, double energy)
+    {
+        this(name, energy, 1.0);
+    }
+
+    public double getEnergy()
+    {
+        return this.energy;
+    }
+
+    public String getUnitName()
+    {
+        return this.unitName;
+    }
+
+    public double getMultiplier()
+    {
+        return this.multiplier;
+    }
+
+
+    /*
+     amount entered is assumed to be in this unit
+     */
+    public double totalEnergy(double amount)
+    {
+        return this.energy * amount;
+    }
+
+    /*
+     amount entered is assumed to be in this unit
+     */
+    public double getAmountInAnotherUnit(double amount, FoodUnit unit)
+    {
+        return amount/this.multiplier*unit.getMultiplier();
+    }
 
 }
